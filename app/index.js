4a22862256b44fa3fd089f0d4842ad6dc030e3c6
abadpab <abadpab@eszar1nm072.emea.adsint.biz>
2018-08-31 12:59:10 +0200

    Parsing the requested path

--- app/index.js
+++ app/index.js
@@ -4,10 +4,24 @@
 
 // Dependencies
 var http = require('http');
+var url = require('url');
 
 // The server should respond to requests with a String
 var server = http.createServer(function(req,res){
+
+  // Get and parse url
+  var parsedURL = url.parse(req.url, true);
+
+  // Get path
+  var path = parsedURL.pathname;
+  var trimmedpath = path.replace(/^\/+|\/+$/g,'');
+
+  // Send response
   res.end("Hello World\n");
+
+  // Log request path
+  console.log('Request received. Path: ' + trimmedpath);
+
 });
 
 // Start server, listening on port 1234
