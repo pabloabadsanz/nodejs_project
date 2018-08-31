8911421c116382049296900334f5443992d3df99
abadpab <abadpab@eszar1nm072.emea.adsint.biz>
2018-08-31 12:45:59 +0200

    Added server listening on port 1234

--- app/index.js
+++ app/index.js
@@ -1,1 +1,16 @@
-console.log("Hello World!");
+/*
+ * Primary file for the API
+ */
+
+// Dependencies
+var http = require('http');
+
+// The server should respond to requests with a String
+var server = http.createServer(function(req,res){
+  res.end("Hello World\n");
+});
+
+// Start server, listening on port 1234
+server.listen(1234, function(){
+  console.log("Server listening on port 1234");
+});
