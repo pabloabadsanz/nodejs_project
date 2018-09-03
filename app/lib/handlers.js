23c8e6f9ebd400f5dd1571ba19887e74dacbee87
abadpab <abadpab@eszar1nm072.emea.adsint.biz>
2018-09-03 14:26:59 +0200

    Added DELETE method to users REST API

--- app/lib/handlers.js
+++ app/lib/handlers.js
@@ -149,8 +149,30 @@ handlers._users.put = function(data, callback) {
 };
 
 // Users - DELETE
+// Required data: phone
+// @TODO Only let an authenticated user delete their object. Don't let them delete anyone else's
+// @TODO Cleanup (delete) any other data files associated with this user
 handlers._users.delete = function(data, callback) {
-
+  // Check that the phone number is valid
+  var phone = typeof(data.queryStringObject.phone) == 'string' && data.queryStringObject.phone.trim().length == 10 ? data.queryStringObject.phone.trim() : false;
+  if (phone) {
+    // Lookup the user
+    _data.read('users', phone, function(err, data) {
+      if (!err && data) {
+        _data.delete('users', phone, function(err) {
+          if (!err) {
+            callback(200);
+          } else {
+            callback(500, {'Error': 'Could not delete the specified user'});
+          }
+        });
+      } else {
+        callback(400, {'Error': 'Could not find the specified user'});
+      }
+    });
+  } else {
+    callback(400, {'Error': 'Missing required field'});
+  }
 };
 
 handlers.ping = function(data,callback){
