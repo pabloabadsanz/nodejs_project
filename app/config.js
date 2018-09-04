e57ae4e7f3d4847f5fc16ae40159acf2c974368d
abadpab <abadpab@eszar1nm072.emea.adsint.biz>
2018-09-04 16:28:21 +0200

    Added SMS gateway with twilio

--- app/config.js
+++ app/config.js
@@ -12,7 +12,12 @@ environments.staging = {
   'httpsPort' : 3001,
   'envName' : 'staging',
   'hashingsecret': 'thisIsASecret',
-  'maxChecks': 5
+  'maxChecks': 5,
+  'twilio' : {
+    'accountSid' : 'ACb32d411ad7fe886aac54c665d25e5c5d',
+    'authToken' : '9455e3eb3109edc12e3d8c92768f7a67',
+    'fromPhone' : '+15005550006'
+  }
 };
 
 // Production environment
@@ -21,7 +26,12 @@ environments.production = {
   'httpsPort' : 1235,
   'envName' : 'production',
   'hashingsecret': 'thisIsASecret',
-  'maxChecks': 5
+  'maxChecks': 5,
+  'twilio' : {
+    'accountSid' : 'ACb32d411ad7fe886aac54c665d25e5c5d',
+    'authToken' : '9455e3eb3109edc12e3d8c92768f7a67',
+    'fromPhone' : '+15005550006'
+  }
 }
 
 // Determine which env was passed as argument
--- app/lib/helpers.js
+++ app/lib/helpers.js
@@ -6,6 +6,8 @@
 // Dependencies
 var crypto = require('crypto');
 var config = require('../config');
+var https = require('https');
+var querystring = require('querystring');
 
  // Container for all the Helpers
  var helpers = {};
@@ -51,6 +53,62 @@ helpers.createRandomString = function(strLength) {
     return false;
   }
 }
+
+// Send an SMS message via Twilio
+helpers.sendTwilioSMS = function(phone, msg, callback) {
+  // Validate parameters
+  phone = typeof(phone) == 'string' && phone.trim().length == 9 ? phone.trim() : false;
+  msg = typeof(msg) == 'string' && msg.trim().length > 0 && msg.trim().length <= 1600 ? msg.trim() : false;
+  if (phone && msg) {
+    // Configure the request payload
+    var payload = {
+      'From': config.twilio.fromPhone,
+      'To': '+34' + phone,
+      'Body': msg
+    };
+
+    var stringpayload = querystring.stringify(payload);
+
+    // Configure request details
+    var requestdetails = {
+      'protocol': 'https:',
+      'hostname': 'api.twilio.com',
+      'method': 'POST',
+      'path': '/2010-04-01/Accounts/' + config.twilio.accountSid + '/Messages.json',
+      'auth': config.twilio.accountSid + ':' + config.twilio.authToken,
+      'headers': {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Content-Length': Buffer.byteLength(stringpayload)
+      }
+    };
+
+    // Instantiate request object
+    var req = https.request(requestdetails, function(res) {
+      // Grab the status of the sent request
+      var status = res.statusCode;
+      // Callback successfully if the request went through
+      if (status == 200 || status == 201) {
+        callback(false);
+      } else {
+        callback('Status code returned was' + status);
+      }
+    });
+
+    // Bind to the error event so it does not get thrown
+    req.on('error', function(e) {
+      callback(e);
+    });
+
+    // Add the payload
+    req.write(stringpayload);
+
+    // End the request
+    req.end();
+
+  } else {
+    callback('Given parameters were missing or invalid');
+  }
+};
 
  // Export the module
  module.exports = helpers;
