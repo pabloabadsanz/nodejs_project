d305594383cfcd4d3dd5c7ca392e2504195b89e9
abadpab <abadpab@eszar1nm072.emea.adsint.biz>
2018-09-05 08:59:27 +0200

    Finished log file workers. Added log rotation and compression to module

--- app/lib/logs.js
+++ app/lib/logs.js
@@ -39,4 +39,103 @@ lib.append = function(file, str, callback) {
   });
 };
 
+// List all the logs and optionally include the compressed logs
+lib.list = function(includeCompressedLogs, callback) {
+  fs.readdir(lib.basedir, function(err, data) {
+    if (!err && data && data.length > 0) {
+      var trimmedFileNames = [];
+      data.forEach(function(fileName) {
+        // Add the .log files
+        if (fileName.indexOf('.log') > -1) {
+          trimmedFileNames.push(fileName.replace('.log', ''));
+        }
+
+        // Add on the .gz files
+        if (fileName.indexOf('.gz.b64') > -1 && includeCompressedLogs) {
+          trimmedFileNames.push(fileName.replace('.gz.b64', ''));
+        }
+      });
+      callback(false, trimmedFileNames);
+    } else {
+      callback(err, data);
+    }
+  });
+};
+
+// Compress the contents of one .log file into a .gz.b64 file within the same directory
+lib.compress = function(logId, newFileId, callback) {
+  var sourceFile = logId + '.log';
+  var destinationFile = newFileId + '.gz.b64';
+
+  // Read the source file
+  fs.readFile(lib.basedir + sourceFile, 'utf8', function(err, inputString) {
+    if (!err && inputString) {
+      // Compress the data using gzip
+      zlib.gzip(inputString, function(err, buffer) {
+        if (!err && buffer) {
+          // Send the data to the destination file
+          fs.open(lib.basedir + destinationFile, 'wx', function(err, fileDescriptor) {
+            if (!err && fileDescriptor) {
+              // Write to file
+              fs.writeFile(fileDescriptor, buffer.toString('base64'), function(err) {
+                if (!err) {
+                  // Close destination file
+                  fs.close(fileDescriptor, function(err) {
+                    if (!err) {
+                      callback(false);
+                    } else {
+                      callback(err);
+                    }
+                  });
+                } else {
+                  callback(err);
+                }
+              });
+            } else {
+              callback(err);
+            }
+          });
+        } else {
+          callback(err);
+        }
+      });
+    } else {
+      callback(err);
+    }
+  })
+};
+
+// Decompress contents of .gz.b64 file into a string variable
+lib.decompress = function(fileId, callback) {
+  var fileName = fileId + '.gz.b64';
+  fs.readFile(lib.basedir + fileName, 'utf8', function(err, str) {
+    if (!err && str) {
+        // Decompress the data
+        var inputBuffer = Buffer.from(str, 'base64');
+        zlib.unzip(inputBuffer, function(err, outputBuffer) {
+          if (!err && outputBuffer) {
+            // Callback
+            var str = outputBuffer.toString();
+            callback(false, str);
+          } else {
+            callback(err);
+          }
+        });
+    } else {
+      callback(err);
+    }
+  });
+};
+
+// Truncate a log file
+lib.truncate = function(logId, callback) {
+  fs.truncate(lib.basedir + logId + '.log', 0, function(err) {
+    if (!err) {
+      callback(false);
+    } else {
+      callback(err);
+    }
+  });
+};
+
 module.exports = lib;
