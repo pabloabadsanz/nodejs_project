d50c21c6add8db841c214254f714cb576355e173
abadpab <abadpab@eszar1nm072.emea.adsint.biz>
2018-09-11 08:58:12 +0200

    Started the logic for logging in

--- app/lib/handlers.js
+++ app/lib/handlers.js
@@ -82,6 +82,39 @@ handlers.accountCreate = function(data, callback) {
   }
 };
 
+// Create new session
+handlers.sessionCreate = function(data, callback) {
+  // Reject any request that isn't a GET
+  if (data.method == 'get') {
+
+    // Prepare data for interpolation
+    var templateData = {
+      'head.title': 'Login to your account',
+      'head.description': 'Please enter your phone number and password to access your account',
+      'body.class': 'sessionCreate'
+    };
+
+    // Read in the index teamplate as a string
+    helpers.getTemplate('sessionCreate', templateData, function(err, str) {
+      if (!err && str) {
+        // Add the universal header and footer
+        helpers.addUniversalTemplates(str, templateData, function(err, str) {
+          if (!err && str) {
+            // Return that page as HTML
+            callback(200, str, 'html');
+          } else {
+            callback(500, undefined, 'html');
+          }
+        });
+      } else {
+        callback(500, undefined, 'html');
+      }
+    });
+  } else {
+    callback(405, undefined, 'html');
+  }
+};
+
 // Favicon
 handlers.favicon = function(data, callback) {
   // Reject any request that isn't a GET
