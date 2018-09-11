1e9c08b0ba12db32d602d2889c3a846afa284a74
abadpab <abadpab@eszar1nm072.emea.adsint.biz>
2018-09-11 08:52:47 +0200

    Created the account create form, linked it with the REST API

--- app/lib/handlers.js
+++ app/lib/handlers.js
@@ -49,6 +49,39 @@ handlers.index = function(data, callback) {
   }
 };
 
+// Create account
+handlers.accountCreate = function(data, callback) {
+  // Reject any request that isn't a GET
+  if (data.method == 'get') {
+
+    // Prepare data for interpolation
+    var templateData = {
+      'head.title': 'Create an account',
+      'head.description': 'Signup is easy and only takes a few seconds.',
+      'body.class': 'accountCreate'
+    };
+
+    // Read in the index teamplate as a string
+    helpers.getTemplate('accountCreate', templateData, function(err, str) {
+      if (!err && str) {
+        // Add the universal header and footer
+        helpers.addUniversalTemplates(str, templateData, function(err, str) {
+          if (!err && str) {
+            // Return that page as HTML
+            callback(200, str, 'html');
+          } else {
+            callback(500, undefined, 'html');
+          }
+        });
+      } else {
+        callback(500, undefined, 'html');
+      }
+    });
+  } else {
+    callback(405, undefined, 'html');
+  }
+};
+
 // Favicon
 handlers.favicon = function(data, callback) {
   // Reject any request that isn't a GET
