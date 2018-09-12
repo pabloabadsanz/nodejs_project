e018f0390de0f747a67142f44c7116e4f4ab7ebf
abadpab <abadpab@eszar1nm072.emea.adsint.biz>
2018-09-12 15:28:30 +0200

    Added the logic and html templates for creating checks

--- app/lib/handlers.js
+++ app/lib/handlers.js
@@ -214,6 +214,38 @@ handlers.accountDeleted = function(data, callback) {
   }
 };
 
+// Create a new check
+handlers.checksCreate = function(data, callback) {
+  // Reject any request that isn't a GET
+  if (data.method == 'get') {
+
+    // Prepare data for interpolation
+    var templateData = {
+      'head.title': 'Create a new check',
+      'body.class': 'checksCreate'
+    };
+
+    // Read in the index teamplate as a string
+    helpers.getTemplate('checksCreate', templateData, function(err, str) {
+      if (!err && str) {
+        // Add the universal header and footer
+        helpers.addUniversalTemplates(str, templateData, function(err, str) {
+          if (!err && str) {
+            // Return that page as HTML
+            callback(200, str, 'html');
+          } else {
+            callback(500, undefined, 'html');
+          }
+        });
+      } else {
+        callback(500, undefined, 'html');
+      }
+    });
+  } else {
+    callback(405, undefined, 'html');
+  }
+};
+
 // Favicon
 handlers.favicon = function(data, callback) {
   // Reject any request that isn't a GET
