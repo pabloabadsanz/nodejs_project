eefc119998c36433ecd9658e917c3a62a3f9c280
pabloabadsanz <pabloabadsanz@gmail.com>
2018-09-16 16:55:36 +0200

    Lint strict mode

---
+++ app/index_strict.js
@@ -0,0 +1,35 @@
+/*
+ * Primary file for the API
+ */
+
+// Dependencies
+var server = require('./lib/server');
+var workers = require('./lib/workers');
+var cli = require('./lib/cli');
+
+// Declare the application
+var app = {};
+
+// Declare a global (the strict mode should catch)
+foo = 'bar';
+
+// Initialization function
+app.init = function() {
+  // Start the server
+  server.init();
+
+  // Start the workers
+  workers.init();
+
+  // Start the CLI, but make sure it starts last
+  setTimeout(function() {
+    cli.init();
+  }, 50);
+
+};
+
+// Execute
+app.init();
+
+// Export the application
+module.exports = app;
